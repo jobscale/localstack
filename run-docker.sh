e6ad5052d5a703751d137fdee77eef7f2b0dc5a0
jobscale <jobscale@users.noreply.github.com>
2024-05-06 08:48:31 +0900

    run-docker.sh added

---
+++ run-docker.sh
@@ -0,0 +1,6 @@
+{
+  docker run --name lo-stack --restart always \
+  -e PERSISTENCE=1 -v $HOME/lo-stack-volume:/var/lib/localstack \
+  -p 4566:4566 -d ghcr.io/jobscale/localstack:debian
+  docker logs --since 5m -f lo-stack
+}
