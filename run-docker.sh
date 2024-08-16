b2d63af89e2f492e21505b5b16696a23b1f30a4a
jobscale <jobscale@users.noreply.github.com>
2024-08-16 17:21:31 +0900

    github/workflow updated

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,9 @@
 {
   docker run --name lo-stack --restart always \
-  -e PERSISTENCE=1 -v $HOME/lo-stack-volume:/var/lib/localstack \
-  -p 4566:4566 -d ghcr.io/jobscale/localstack:debian
+  --memory 1g \
+  -e PERSISTENCE=1 \
+  -v $HOME/lo-stack-volume:/var/lib/localstack \
+  -p 4566:4566 \
+  -d ghcr.io/jobscale/localstack:debian
   docker logs --since 5m -f lo-stack
 }
